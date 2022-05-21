11af3fafbdebe7a7cfe05094da3d6cf8310d7e5d
Kevin <kevin26428@gmail.com>
2022-05-21 13:15:11 -0500

    add basic logic for when a room starts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,128 +1,61 @@
 import React, { useState, useEffect } from 'react';
 import {
-  ThemeProvider, Typography, Stack, Box, List, ListItem, ListItemText, Paper, Snackbar, Alert, Fade,
+  AppBar, Toolbar, Typography, Button, Stack,
 } from '@mui/material';
-
+import { ThemeProvider } from '@mui/material/styles';
 import client, { events } from '@urturn/client';
+import Editor from '@monaco-editor/react';
 import theme from './theme';
 
-// prevent rerendering tictactoe row and entries that are the same value
-const getRowKey = (row, rowNum) => `${rowNum}-${row.join('-')}`;
-const getColKey = (val, colNum) => `${colNum}-${val}`;
-
-const getStatusMsg = ({
-  status, winner, finished, plrToMove,
-}) => {
-  if (finished) {
-    if (winner) {
-      return `${winner} won the game!`;
-    }
-    return "It's a tie!";
-  } if (status === 'preGame') {
-    return 'Waiting on another player...';
-  } if (status === 'inGame') {
-    return `Waiting on player ${plrToMove} to make their move...`;
-  }
-  return 'Error: You should never see this. Contact developers!';
-};
+const DEFAULT_MOVE_TEXT = '// put move JSON here';
 
 function App() {
-  const [boardGame, setBoardGame] = useState(client.getBoardGame() || {});
-  console.log('boardGame:', boardGame);
+  const [moveObj, setMoveObj] = useState(null);
   useEffect(() => {
-    const onStateChanged = (newBoardGame) => {
-      setBoardGame(newBoardGame);
-    };
-    events.on('stateChanged', onStateChanged);
+    events.on('stateChanged', console.log);
     return () => {
-      events.off('stateChanged', onStateChanged);
+      events.off('stateChanged', console.log);
     };
   }, []);
-
-  const [recentErrorMsg, setRecentErrorMsg] = useState(null);
-
-  const {
-    state: {
-      board,
-      status,
-      winner,
-      plrToMoveIndex,
-    } = {
-      board: [
-        [null, null, null],
-        [null, null, null],
-        [null, null, null],
-      ],
-    },
-  } = boardGame;
-  const { players = [], finished } = boardGame;
-  const generalStatus = getStatusMsg({
-    status, winner, finished, plrToMove: status === 'inGame' ? players[plrToMoveIndex] : null,
-  });
-
   return (
     <ThemeProvider theme={theme}>
-      <Stack spacing={1} sx={{ justifyContent: 'center' }}>
-        <Typography variant="h3" textAlign="center" color="text.primary">TicTacToe</Typography>
-        <Typography textAlign="center" color="text.primary">{generalStatus}</Typography>
-        <Stack margin={2} spacing={1} direction="row" justifyContent="center">
-          <Box>
-            {board.map((row, rowNum) => (
-              <Stack key={getRowKey(row, rowNum)} direction="row">
-                {row.map((val, colNum) => (
-                  <Stack
-                    key={getColKey(val, colNum)}
-                    direction="row"
-                    justifyContent="center"
-                    alignItems="center"
-                    sx={{
-                      border: 1,
-                      borderColor: 'text.primary',
-                      height: '100px',
-                      width: '100px',
-                    }}
-                    onClick={async (event) => {
-                      event.preventDefault();
-                      const move = { x: rowNum, y: colNum };
-                      const { error } = await client.makeMove(move);
-                      if (error) {
-                        setRecentErrorMsg(error.message);
-                      }
-                    }}
-                  >
-                    <Typography color="text.primary" fontSize="60px">
-                      {val}
-                    </Typography>
-                  </Stack>
-                ))}
-              </Stack>
-            ))}
-          </Box>
-          <Paper>
-            <Stack padding={1} sx={{ minWidth: '100px' }}>
-              <Typography disableGutter color="text.primary">Players</Typography>
-              <List dense disablePadding padding={0}>
-                {players.map((player, ind) => (
-                  <ListItem dense disablePadding key={player}>
-                    <ListItemText primary={`${ind + 1}: ${player}`} />
-                  </ListItem>
-                ))}
-              </List>
+      <Stack height="100vh">
+        <AppBar position="relative">
+          <Toolbar variant="dense" sx={{ justifyContent: 'space-between' }}>
+            <Typography color="text.primary">
+              Test Game Frontend
+            </Typography>
+            <Stack spacing={1} direction="row">
+              <Button
+                size="small"
+                variant="outlined"
+                onClick={async () => {
+                  const result = await client.makeMove(moveObj);
+                  console.log('result from move:', { moveObj, result });
+                }}
+              >
+                Make Move
+              </Button>
             </Stack>
-          </Paper>
+          </Toolbar>
+        </AppBar>
+        <Stack sx={{ flexGrow: 1 }}>
+          <Editor
+            onChange={(jsonStr) => {
+              try {
+                const jsonObj = JSON.parse(jsonStr);
+                setMoveObj(jsonObj);
+              } catch (e) {
+                setMoveObj(null);
+              }
+            }}
+            height="100%"
+            defaultLanguage="json"
+            defaultValue={DEFAULT_MOVE_TEXT}
+            theme="vs-dark"
+          />
         </Stack>
       </Stack>
-      <Snackbar
-        autoHideDuration={6000}
-        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-        open={recentErrorMsg !== null}
-        onClose={() => { setRecentErrorMsg(null); }}
-        TransationComponent={Fade}
-      >
-        <Alert severity="error" sx={{ width: '100%' }}>
-          {recentErrorMsg}
-        </Alert>
-      </Snackbar>
     </ThemeProvider>
   );
 }
--- index.js
+++ index.js
@@ -1,74 +1,49 @@
-// TicTacToe Example
+// Avalon Example
+// TODO: evil players and merlin are the only players that should know who good and evil players are
+// however this information is broadcasted to all players
+
 const Status = Object.freeze({
   PreGame: 'preGame',
   InGame: 'inGame',
   EndGame: 'endGame',
 });
 
-function getPlrMark(plr, plrs) {
-  if (plr === plrs[0]) {
-    return 'X';
-  }
-  return 'O';
-}
+const Winner = Object.freeze({
+  Good: 'good',
+  Evil: 'evil',
+});
 
-function getPlrFromMark(mark, plrs) {
-  return mark === 'X' ? plrs[0] : plrs[1];
-}
+// map from total number of players to the number of players required for each quest
+const QUEST_PLAYERS_COUNT = {
+  5: [2, 3, 2, 3, 3],
+  6: [2, 3, 4, 3, 4],
+  7: [2, 3, 3, 4, 4],
+  8: [3, 4, 4, 5, 5],
+  9: [3, 4, 4, 5, 5],
+};
 
-function isWinningSequence(arr) {
-  return arr[0] != null && arr[0] === arr[1] && arr[1] === arr[2];
-}
-
-function isEndGame(board, plrs) {
-  // check if there is a winner
-  for (let i = 0; i < board.length; i += 1) {
-    const row = board[i];
-    const col = [board[0][i], board[1][i], board[2][i]];
-
-    if (isWinningSequence(row)) {
-      return [true, getPlrFromMark(row[0], plrs)];
-    } if (isWinningSequence(col)) {
-      return [true, getPlrFromMark(col[0], plrs)];
-    }
-  }
-
-  const d1 = [board[0][0], board[1][1], board[2][2]];
-  const d2 = [board[0][2], board[1][1], board[2][0]];
-  if (isWinningSequence(d1)) {
-    return [true, getPlrFromMark(d1[0], plrs)];
-  } if (isWinningSequence(d2)) {
-    return [true, getPlrFromMark(d2[0], plrs)];
-  }
-
-  // check for tie
-  if (board.some((row) => row.some((mark) => mark === null))) {
-    return [false, null];
-  }
-  return [true, null];
-}
+// map from total number of players to the number of evil players
+// we can deduce the number of good players to be the total minus the number of evil players
+const EVIL_PLAYERS_COUNT = {
+  5: 2,
+  6: 2,
+  7: 3,
+  8: 3,
+  9: 3,
+  10: 4,
+};
 
 /**
- * Generic board game types
- * @type BoardGame: json object, in the format of
- * {
- *  // creator read write fields
- *  state: json object, which represents any board game state
- *  joinable: boolean (default=true), whether or not the room can have new players added to it
- *  finished: boolean (default=false), when true there will be no new board game state changes
- *
- *  // creator read only
- *  players: [string], array of unique playerIds
- *  version: Number, an integer value that increases by 1 with each state change
- * }
- * @type BoardGameResult: json object, in the format of
- * {
- *  // fields that creator wants to overwrite
- *  state?: json object, which represents any board game state
- *  joinable?: boolean, whether or not the room can have new players added to it
- *  finished?: boolean, when true there will be no new board game state changes
- * }
+ * shuffle will take an array and create a new shuffled array from the previous elements
+ * @param {any[]} unshuffledArr is an array to be shuffled
+ * @returns {any[]} the shuffled array
  */
+function shuffle(unshuffledArr) {
+  return unshuffledArr
+    .map((value) => ({ value, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ value }) => value);
+}
 
 /**
  * onRoomStart
@@ -76,14 +51,9 @@ function isEndGame(board, plrs) {
  */
 function onRoomStart() {
   return {
+    // Most of the state is actually initialized when enough players join, this state serves
     state: {
       status: Status.PreGame,
-      board: [
-        [null, null, null],
-        [null, null, null],
-        [null, null, null],
-      ],
-      winner: null, // null means tie if game is finished, otherwise set to the plr that won
     },
   };
 }
@@ -97,21 +67,33 @@ function onRoomStart() {
 function onPlayerJoin(plr, boardGame) {
   const { players, state } = boardGame;
 
-  // VALIDATIONS
-  // check if game has started
-  if (state.status !== Status.PreGame) {
-    throw new Error("game has already started, can't join the game!");
-  }
-
-  // TRANSFORMATIONS
   // determine if we should start the game
-  if (players.length === 2) {
-    // start game
+  if (players.length === 5) {
+    const evilCount = EVIL_PLAYERS_COUNT[players.length];
+    const goodCount = players.length - evilCount;
+
+    // shuffle the players to randomize who is good and evil and who is merlin
+    const shuffledPlayers = shuffle(players);
+    state.playerToData = shuffledPlayers.reduce((playerToData, player, curInd) => ({
+      ...playerToData,
+      [player]: {
+        good: curInd < goodCount,
+        merlin: curInd === 0,
+      },
+    }), {});
+
+    // we can get the current leader who does the team building as shuffledPlayers[indexOfLeader]
+    state.indexOfLeader = 0;
+    state.shuffledPlayers = shuffledPlayers;
+
+    // the indexOfQuest provides a nice way of grabbing the total number of players needed
+    // on the current quest by doing QUEST_PLAYERS_COUNT[players.length][indexOfQuest]
+    // we can get the quest count by doing indexOfQuest + 1
+    state.indexOfQuest = 0;
     state.status = Status.InGame;
-    state.plrToMoveIndex = 0;
     return {
       state,
-      joinable: false,
+      joinable: false, // in Avalon, new players cannot join mid game
     };
   }
   return {
@@ -130,50 +112,19 @@ function onPlayerJoin(plr, boardGame) {
 function onPlayerMove(plr, move, boardGame) {
   const { state, players } = boardGame;
   const { board, plrToMoveIndex } = state;
-
-  // VALIDATIONS
-  // boardgame must be in the game
-  const { x, y } = move;
-  if (state.status !== Status.InGame) {
-    throw new Error("game is not in progress, can't make move!");
-  }
-  if (players[plrToMoveIndex] !== plr) {
-    throw new Error(`Its not this player's turn: ${plr}`);
-  }
-  if (board[x][y] !== null) {
-    throw new Error(`Invalid move, someone already marked here: ${x},${y}`);
-  }
-
-  const plrMark = getPlrMark(plr, players);
-  board[x][y] = plrMark;
-
-  // Check if game is over
-  const [isEnd, winner] = isEndGame(board, players);
-  if (isEnd) {
-    state.status = Status.EndGame;
-    state.winner = winner;
-    return { state, finished: true };
-  }
-
-  state.plrToMoveIndex = plrToMoveIndex === 0 ? 1 : 0;
-  return { state };
 }
 
 /**
  * onPlayerQuit
+ * If a player quits in Avalon, then the game immediately ends with no winners.
  * @param {string} plr string, which represents the player id
  * @param {BoardGame} currentGame
  * @returns {BoardGameResult}
  */
 function onPlayerQuit(plr, boardGame) {
-  const { state, players } = boardGame;
+  const { state } = boardGame;
   state.status = Status.EndGame;
-  if (players.length === 1) {
-    const [winner] = players;
-    state.winner = winner;
-    return { state, joinable: false, finished: true };
-  }
-  return { joinable: false, finished: true };
+  return { state, finished: true };
 }
 
 module.exports = {
